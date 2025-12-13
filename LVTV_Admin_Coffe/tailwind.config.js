ce6916028764a95734d0ecf3f4fd3133f7085163
hoangminhit203 <hoangminhit203@gmail.com>
2025-12-13 08:33:53 +0700

    Update wed Admin

--- LVTV_Admin_Coffe/tailwind.config.js
+++ LVTV_Admin_Coffe/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
